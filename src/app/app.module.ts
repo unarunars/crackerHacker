1455bccd3f45d835833bdae5c412b2b7e540024d
unarunars <unr2@hi.is>
2020-04-10 15:36:22 +0000

    input value

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  values = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onKey(event: any) { // without type info
+    //this.values += event.target.value;
+    this.values = event.target.value;
+    console.log(this.values);
   }
 
 }
