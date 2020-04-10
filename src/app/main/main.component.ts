51ae66b3b96c6dd64619e34111489a5bf02cd893
unarunars <unr2@hi.is>
2020-04-10 19:09:53 +0000

    ekkert spes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SkipSelf } from '@angular/core';
 import { createRainbow } from 'src/assets/js/rainbow.js';
 import { checkPassword } from 'src/assets/js/crack.js';
 
@@ -37,15 +37,65 @@ export class MainComponent implements OnInit {
 
     }else{
       console.log(value.length);
-      var time = this.showTime(value.length);
+      var time = this.showTime(value);
       this.title = "The time it took to find your password is"
+      if(time < 0){
+        var mill = time / 1000;
+        this.text = mill + " milliseconds";
+      }else if(time >= 60  && time < 3600){
+        var min = time / 60;
+        this.text = min + " minutes"
+      }else if(time >= 3600 && time < 86400){
+        var hour = time / 3600;
+        this.text = hour + " hours";  
+      }/*else if(time >=86400 && time < 604800){
+        var day = time /93600;
+        this.text = month + " week";
+      }else if( time >= 604800 && time < 1209600){
+        var half
+      }
+      this.text = time + " sek";*/
+      this.text = "afhverju kemur þetta ekki args";
     }
     
   }
-  showTime(length: number){
+  showTime(value : string){
     console.log(length);
-    var alphabet = 68;
-    var results = Math.pow(alphabet,length);
+    var character = "";
+    var alphabet = 0;
+    var hasNumber = false;
+    var hasUpper = false;
+    var hasLower = false;
+    var hasSympol = false;
+    
+
+    //alphabet lengd mismunandi
+    //0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&*?
+    for(let i = 0; i< value.length; i++){
+      character = value.charAt(i);
+      console.log(character);
+      console.log(parseInt(character));
+      if(character === character.toUpperCase() && !hasUpper){
+        alphabet += 26;
+        hasUpper =true;
+      }else if(character === character.toLowerCase() && !hasLower){
+        alphabet +=26;
+        hasLower= true;
+      }
+      else if((character ==='#' || character === '$' || character === '%'|| character == '&' || character == '*') && !hasSympol ){
+        alphabet += 5;
+        hasSympol = true;
+      }else if(parseInt(character) < 10  && !hasNumber){
+        console.log("tala");
+        console.log(character);
+        alphabet += 10;
+        hasNumber = true;
+      }
+      
+    }
+    console.log("alphaber: ")
+    console.log(alphabet)
+    var results = Math.pow(alphabet,value.length);
     results /= 10000000000000;
     console.log(results); 
     return results;
