4bc20fbae84364b3c3912351e66404052b193816
unarunars <unr2@hi.is>
2020-04-10 16:09:31 +0000

    bara testa

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -1,3 +1,4 @@
+import { callbackify } from "util";
 
 export function test() {
     var abc = "ABC";
@@ -6,4 +7,25 @@ export function test() {
         finalString += abc.charCodeAt(i);
 
     console.log(parseInt(finalString));
+
+    //una að prufa
+    function readTextFile(file, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.overrideMimeType("application/json")
+        xhr.open('GET', file , true);
+        console.log("haló");
+        if(xhr.readyState === 4 && xhr.status === "200"){
+            callbackify(xhr.responseText)
+        }
+        xhr.send(null);
+        console.log(xhr.status);
+        console.log(xhr.readyState);
+    }
+    readTextFile("src/assets/js/rainbow.txt", function(text){
+        var data = JSON.parse(text);
+        console.log("þetta á að vera data:");
+        console.log(data);
+    })
+    
+    
 }
