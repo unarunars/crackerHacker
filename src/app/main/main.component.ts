f00a227e8c4066b6bc937df94aa2e047eb63de8b
unarunars <unr2@hi.is>
2020-04-13 21:20:08 +0000

    pulla

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { createRainbow } from 'src/assets/js/rainbow.js';
 import { checkPassword } from 'src/assets/js/crack.js';
 
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -25,23 +26,15 @@ export class MainComponent implements OnInit {
     this.getCrackerHacker(this.value);
   }
   getCrackerHacker(value: string){
-    let obj = checkPassword(value);
-    console.log(obj);
-    //let obj = checkPassword(value);
-    //console.log(obj);
-   /* if(obj.successful){
-      console.log("það fannst");
-      this.title = "It was immediately";
-      if(obj.how === 'rainbow'){
-        this.text = "It was found in a list of known passwords";
-      }
-
-    }else{
-      console.log(value.length);
+   
       var time = this.showTime(value);
+      console.log(time < 0);
+      console.log(Math.sign(time));
       console.log(time);
-      this.title = "The time it took to find your password is"
-      if(time < 0){
+      console.log(time.toFixed());
+      console.log(time);
+      this.title = "The time it took to find your password is";
+     /* if(time < 0){
         var mill = time / 1000;
         this.text = mill + " milliseconds";
       }else if(time >= 60  && time < 3600){
@@ -50,14 +43,17 @@ export class MainComponent implements OnInit {
       }else if(time >= 3600 && time < 86400){
         var hour = time / 3600;
         this.text = hour + " hours";  
-      }/*else if(time >=86400 && time < 604800){
+      }
+      */
+      this.text = "afhverju kemur þetta ekki args";
+      
+      /*else if(time >=86400 && time < 604800){
         var day = time /93600;
         this.text = month + " week";
       }else if( time >= 604800 && time < 1209600){
         var half
       }
       this.text = time + " sek";
-      this.text = "afhverju kemur þetta ekki args";
     }*/
     
   }
@@ -69,14 +65,11 @@ export class MainComponent implements OnInit {
     var hasUpper = false;
     var hasLower = false;
     var hasSympol = false;
-    
-
     //alphabet lengd mismunandi
     //0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&*?
     for(let i = 0; i< value.length; i++){
       character = value.charAt(i);
-      console.log(character);
-      console.log(parseInt(character));
+      
       if(character === character.toUpperCase() && !hasUpper){
         alphabet += 26;
         hasUpper =true;
@@ -88,19 +81,17 @@ export class MainComponent implements OnInit {
         alphabet += 5;
         hasSympol = true;
       }else if(parseInt(character) < 10  && !hasNumber){
-        console.log("tala");
         console.log(character);
         alphabet += 10;
         hasNumber = true;
       }
       
     }
-    console.log("alphaber: ")
-    console.log(alphabet)
+    console.log("results er: ", alphabet + "^"+ value.length);
     var results = Math.pow(alphabet,value.length);
+    /*console.log(results);*/
     //erum að gera ráð fyrir að talvan sé mjög öflug
     results /= 10000000000000;
-    console.log(results); 
     return results;
     
   }
