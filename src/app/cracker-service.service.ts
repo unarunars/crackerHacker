27f36a1718eaadbf8b8ee7179033ba9298b037e2
unarunars <unr2@hi.is>
2020-04-16 18:13:01 +0000

    temp

--- src/app/cracker-service.service.ts
+++ src/app/cracker-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CrackerServiceService {
 
-  constructor() { }
+  getJson(): Observable<any>{
+    
+    return this.http.get('https://aust-hugbo1.herokuapp.com/locations');
+    }
+
+  constructor(
+    private http: HttpClient,
+  ) { }
 }
